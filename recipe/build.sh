3fee6e3788c0d728ef5b5b47a23a52c930d1f757
Mervin Fansler <mervin.fansler@bric.ku.dk>
2025-09-30 15:52:20 +0200

    macos sdk min

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
-export PKG_CPPFLAGS="-D_LIBCPP_DISABLE_AVAILABILITY ${PKG_CPPFLAGS}"
+
+if [ "$(uname)" == "Darwin" ]; then
+  export PKG_CPPFLAGS="${PKG_CPPFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -mmacosx-version-min=10.15"
+fi
 
 sed -i.bak 's/${PROJ_LIBS}/${PROJ_LIBS} ${LDFLAGS}/g' configure
 sed -i.bak 's/${LIBS}/${LIBS} ${LDFLAGS}/g' configure
