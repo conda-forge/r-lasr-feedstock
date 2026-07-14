7ec34aff2c59d3f8487568a260ecbb44abdcf43d
Mervin Fansler <mervin.fansler@bric.ku.dk>
2026-07-14 14:52:00 +0200

    simplify

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
 
-if [ "$(uname)" == "Darwin" ]; then
-  sed -i.bak 's/-mmacosx-version-min=10.13//g' "${PREFIX}/lib/R/etc/Makeconf"
-  export PKG_CPPFLAGS="${PKG_CPPFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY -mmacosx-version-min=11.0"
-fi
-
 sed -i.bak 's/${PROJ_LIBS}/${PROJ_LIBS} ${LDFLAGS}/g' configure
 sed -i.bak 's/${LIBS}/${LIBS} ${LDFLAGS}/g' configure
 
